98403329aaac1a8a595950a25497a03451b8e477
Dimitar Kolev <dimitarkolev00@gmail.com>
2020-03-11 17:22:12 +0200

    Initial push

---
+++ Final Exam/P01.Lootbox/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P01.Lootbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] firstBoxInput = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int[] secondBoxInput = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Queue<int> firstQueue = new Queue<int>(firstBoxInput);
+            Stack<int> secondSkack = new Stack<int>(secondBoxInput);
+
+            List<int> claimedItemsSum = new List<int>();
+
+
+            while (firstQueue.Count != 0 && secondSkack.Count != 0)
+            {
+                int currentFirst = firstQueue.Peek();
+                int currentSecond = secondSkack.Peek();
+
+                int sum = currentFirst + currentSecond;
+
+                if (sum % 2 == 0)
+                {
+                    claimedItemsSum.Add(sum);
+                    firstQueue.Dequeue();
+                    secondSkack.Pop();
+                }
+                else
+                {
+                    int itemToMove = currentSecond;
+                    firstQueue.Enqueue(itemToMove);
+                    secondSkack.Pop();
+                }
+            }
+            if (firstQueue.Count <= 0)
+            {
+                Console.WriteLine("First lootbox is empty");
+            }
+            else if (secondSkack.Count <= 0)
+            {
+                Console.WriteLine("Second lootbox is empty");
+            }
+            int totalSum = claimedItemsSum.Sum();
+            if (totalSum >= 100)
+            {
+                Console.WriteLine($"Your loot was epic! Value: {totalSum}");
+            }
+            else
+            {
+                Console.WriteLine($"Your loot was poor... Value: {totalSum}");
+            }
+        }
+    }
+}
---
+++ Final Exam/P03.Guilt/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+
+namespace Guild
+{
+    public class Player
+    {
+        private Player()
+        {
+            this.Rank = "Trial";
+            this.Description = "n/a";
+        }
+        public Player(string name , string clas)
+        {
+            this.Name = name;
+            this.Class = clas;
+        }
+       
+        public string Name { get; set; }
+        public string Class { get; set; }
+        public string Rank { get { return this.Rank = "Trial"; } set { } }
+        public string Description { get { return this.Description = "n/a"; } set { } }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb
+                .AppendLine($"Player { this.Name}: { this.Class}")
+                .AppendLine($"Rank: {this.Rank}")
+                .AppendLine($"Description: {this.Description}");
+
+            return sb.ToString().TrimEnd(); ;
+        }
+    }
+}
